72a0369a3582905c781fece361a7bc329dd0d5d5
Vinay-patle <115407891+Vinay-patle@users.noreply.github.com>
2024-08-23 21:03:32 +0530

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -4,59 +4,56 @@ export const Work = [
     {
     id: 1,
     name: "Event Organizer",
-    description:
-        "It is build on top of the HTML,CSS and JS. ",
+    description:"It is build on top of the HTML,CSS and JS. ",
     tags: ["HTML", "CSS", "JAVASCRIPT"],
-
     demo: "https://agency-website-eta.vercel.app/",
     github: "https://github.com/vinay-patle/wedlock",
     },
+
     {
     id: 2,
     name: "Weather forcast  website",
-    description:
-        "A Weather forcast website build in ReactJS. With searching and filtering functionalities.",
+    description:"A Weather forcast website build in ReactJS. With searching and filtering functionalities.",
     tags: ["react", "sass"],
-
     demo: "http://react-job-listing-website.codebucks27.vercel.app/",
     github: "https://github.com/codebucks27/react-job-listing-website",
   },
+
   {
     id: 3,
     name: "Maillard",
     description: "A Restrunt website for table booking .",
     tags: ["react", "api", "sass"],
-
     demo: "http://devto-clone-codebucks27.vercel.app/",
     github: "https://github.com/codebucks27/devto-clone",
   },
+
   {
     id: 4,
     name: "Sidebar Menu",
     description:
       "A cool looking sidebar menu build using HTML, CSS and JS. It has really smooth animations.",
     tags: ["Html", "Css" , "Js"],
-
     demo: "https://react-sidebar.vercel.app/",
     github: "https://github.com/codebucks27/react-sidebar",
   },
+
   {
     id: 5,
     name: "Plot listing  website",
     description:
       "A Plot listing  website build using Html, Css, JS With searching and filtering functionalities.",
     tags: ["react", "redux", "framer-motion"],
-
     demo: "http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
     github: "https://github.com/codebucks27/React-Redux-Todo-App",
   },
+
   {
     id: 6,
     name: "Anshul Photographer",
     description:
       "A  Photographer website build using Html, Css and JS built from scratch.",
     tags: ["react", "react-router", "css"],
-
     demo: "http://react-responsive-navbar-codebucks27.vercel.app/",
     github: "https://github.com/codebucks27/React-responsive-navbar",
   },
