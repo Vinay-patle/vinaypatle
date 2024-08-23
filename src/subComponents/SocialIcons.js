40bf39eed8ac214771439305fa3dca83548db8d5
Vinay-patle <115407891+Vinay-patle@users.noreply.github.com>
2024-08-24 02:42:37 +0530

    Update SocialIcons.js

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import React from "react";
 // import { NavLink } from 'react-router-dom'
 import styled from "styled-components";
-import { Facebook, Github, Twitter, YouTube } from "../components/AllSvgs";
+import { Facebook, Github, Linkedin, Twitter, YouTube } from "../components/AllSvgs";
 import { DarkTheme } from "../components/Themes";
 
 const Icons = styled.div`
@@ -31,6 +31,24 @@ const Line = styled(motion.span)`
 const SocialIcons = (props) => {
   return (
     <Icons>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          //target="_blank"
+          href={"https://github.com/vinay-patle"}
+        >
+          <Linkedin
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+
       <motion.div
         initial={{ scale: 0 }}
         animate={{ scale: [0, 1, 1.5, 1] }}
